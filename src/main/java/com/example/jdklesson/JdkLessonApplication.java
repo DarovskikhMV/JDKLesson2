897cd7fd2be953417b64e18dccee25f467012804
DarovskikhMV <doktormakc84@mail.ru>
2024-05-05 21:53:13 +0300

    first commit

---
+++ src/main/java/com/example/jdklesson/JdkLessonApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdklesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdkLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdkLessonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jdklesson/JdkLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdklesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdkLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
